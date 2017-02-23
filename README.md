66b520d8c344789597358d14bf620267a7b64b34
MaksymI <maksym.izmailov.lv@gmail.com>
2017-02-23 19:49:20 +0200

    added bootstrap formatting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Angular_Todolist
 Simple AngularJS (1.6.2) To Do List
+(with bootstrap, gulp and bower ;) )
--- app/js/todo.js
+++ app/js/todo.js
@@ -1,28 +1,43 @@
-angular.module('todoApp', [])
-  .controller('TodoListController', function() {
-    var todoList = this;
-    todoList.todos = [
-      {text:'learn AngularJS', done:true},
-      {text:'build an AngularJS app', done:false}];
- 
-    todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
-      todoList.todoText = '';
+var app = angular.module('todoApp', []);
+app.controller('todoListCtrl', function () {
+    var todoListCtrl = this;
+    todoListCtrl.todos = [
+        {
+            text: 'learn AngularJS',
+            done: true
+        }
+        , {
+            text: 'build first AngularJS app',
+            done: true
+        }
+        , {
+            text: 'start working as junior AngularJS dev',
+            done: false
+        }];
+    todoListCtrl.addTodo = function () {
+        var toPush = {
+            text: todoListCtrl.todoText,
+            done: false
+        };
+        if (todoListCtrl.todoText) {
+            todoListCtrl.todos.push(toPush);
+            todoListCtrl.todoText = '';
+        } else {
+            console.log('input empty');
+        };
     };
- 
-    todoList.remaining = function() {
-      var count = 0;
-      angular.forEach(todoList.todos, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
+    todoListCtrl.remaining = function () {
+        var count = 0;
+        angular.forEach(todoListCtrl.todos, function (todo) {
+            count += todo.done ? 0 : 1;
+        });
+        return count;
     };
- 
-    todoList.archive = function() {
-      var oldTodos = todoList.todos;
-      todoList.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) todoList.todos.push(todo);
-      });
+    todoListCtrl.archive = function () {
+        var oldTodos = todoListCtrl.todos;
+        todoListCtrl.todos = [];
+        angular.forEach(oldTodos, function (todo) {
+            if (!todo.done) todoListCtrl.todos.push(todo);
+        });
     };
-  });
+});
